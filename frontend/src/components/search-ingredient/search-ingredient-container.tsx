5ee5121665c9a172e74c3207bf6eeea1494b6396
seshlol <seshlol@icloud.com>
2020-06-13 11:02:51 +0500

    minor changes

--- frontend/src/components/search-ingredient/search-ingredient-container.tsx
+++ frontend/src/components/search-ingredient/search-ingredient-container.tsx
@@ -1,11 +1,14 @@
+import './search-ingredient-container.scss';
+
 import * as React from 'react';
 
-import './search-ingredient-container.scss';
 import Button from 'react-bootstrap/Button';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
+
 import {FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faSearch} from '@fortawesome/free-solid-svg-icons/faSearch';
 
 export default class SearchIngredientContainer extends React.Component {
 
@@ -16,7 +19,7 @@ export default class SearchIngredientContainer extends React.Component {
                     <FormControl placeholder="ingredient name"/>
                     <InputGroup.Append>
                         <Button>
-                            <FontAwesomeIcon icon="search"/>
+                            <FontAwesomeIcon icon={faSearch}/>
                         </Button>
                     </InputGroup.Append>
                 </InputGroup>

dc68a94d0a3fc001a9dbd3b6f2d13a74d37b219a
Seshlol <seshlol@icloud.com>
2020-08-06 00:42:41 +0500

    started backend

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './components/app/index';
 import store from './store/store';
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>,
     document.getElementById('root')
 );
